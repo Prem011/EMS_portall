bec15db4ded2a4e1cbfae08742141218ff2e9bb1
Prem <pwanjari761@gmail.com>
2024-10-22 23:56:16 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Register from './components/Register'
-import Login from './components/Login'
-import {Routes, Route, Navigate} from 'react-router-dom'
-import Dashboard from './employeesComponents/Dashboard'
-import EmployeesList from './employeesComponents/EmployeesList'
-import CreateEmployee from './employeesComponents/CreateEmployee'
-import UpdateEmployee from './employeesComponents/UpdateEmployee'
-import { useAuth } from './context/AuthProvider'
-import PayrollList from './payrollComponents/PayrollList'
-import Attendence from './employeesComponents/Attendence'
-import CreatePayroll from './payrollComponents/CreatePayroll'
-import UpdatePayroll from './payrollComponents/UpdatePayroll'
-import UpdateAttendence from './attendenceComponents/UpdateAttendence'
+import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './employeesComponents/Dashboard';
+import EmployeesList from './employeesComponents/EmployeesList';
+import CreateEmployee from './employeesComponents/CreateEmployee';
+import UpdateEmployee from './employeesComponents/UpdateEmployee';
+import { useAuth } from './context/AuthProvider';
+import PayrollList from './payrollComponents/PayrollList';
+import Attendence from './employeesComponents/Attendence';
+import CreatePayroll from './payrollComponents/CreatePayroll';
+import UpdatePayroll from './payrollComponents/UpdatePayroll';
+import UpdateAttendence from './attendenceComponents/UpdateAttendence';
 import CreateAttendence from './attendenceComponents/CreateAttendence';
-import ReadAttendance from './attendenceComponents/ReadAttendence'
+import ReadAttendance from './attendenceComponents/ReadAttendence';
 
-const ProtectedRoute = ({ element, ...rest }) => {
+// ProtectedRoute component that checks if user is authenticated
+const ProtectedRoute = ({ element }) => {
     const { authUser } = useAuth();
     return authUser ? element : <Navigate to="/login" />;
 };
@@ -25,54 +26,18 @@ const App = () => {
 
     return (
         <Routes>
-            <Route
-                path='/'
-                element={<ProtectedRoute element={<Dashboard />} />}
-            />
-            <Route
-                path='/login'
-                element={authUser ? <Navigate to="/" /> : <Login />}
-            />
-            <Route
-                path='/register'
-                element={authUser ? <Navigate to="/" /> : <Register />}
-            />
-            <Route
-                path='/employeesDetails'
-                element={<ProtectedRoute element={<EmployeesList />} />}
-            />
-            <Route
-                path='/createEmployee'
-                element={<ProtectedRoute element={<CreateEmployee />} />}
-            />
-            <Route
-                path='/updateEmployee/:id'
-                element={<ProtectedRoute element={<UpdateEmployee />} />}
-            />
-            <Route
-                path='/payroll/:id'
-                element={<ProtectedRoute element={<PayrollList />} />}
-            />
-            <Route
-                path='/createPayroll/:id'
-                element={<ProtectedRoute element={<CreatePayroll/>} />}
-            />
-            <Route
-                path='/updatePayroll/:id'
-                element={<ProtectedRoute element={<UpdatePayroll/>} />}
-            />
-            <Route
-                path='/attendence/:id'
-                element={<ProtectedRoute element={<ReadAttendance />} />}
-            />
-            <Route
-                path='/createAttendance/:id'
-                element={<ProtectedRoute element={<CreateAttendence/>} />}
-            />
-            <Route
-                path='/updateAttendance/:id'
-                element={<ProtectedRoute element={<UpdateAttendence/>} />}
-            />
+            <Route path="/" element={<ProtectedRoute element={<Dashboard />} />} />
+            <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+            <Route path="/register" element={authUser ? <Navigate to="/" /> : <Register />} />
+            <Route path="/employeesDetails" element={<ProtectedRoute element={<EmployeesList />} />} />
+            <Route path="/createEmployee" element={<ProtectedRoute element={<CreateEmployee />} />} />
+            <Route path="/updateEmployee/:id" element={<ProtectedRoute element={<UpdateEmployee />} />} />
+            <Route path="/payroll/:id" element={<ProtectedRoute element={<PayrollList />} />} />
+            <Route path="/createPayroll/:id" element={<ProtectedRoute element={<CreatePayroll />} />} />
+            <Route path="/updatePayroll/:id" element={<ProtectedRoute element={<UpdatePayroll />} />} />
+            <Route path="/attendence/:id" element={<ProtectedRoute element={<ReadAttendance />} />} />
+            <Route path="/createAttendance/:id" element={<ProtectedRoute element={<CreateAttendence />} />} />
+            <Route path="/updateAttendance/:id" element={<ProtectedRoute element={<UpdateAttendence />} />} />
         </Routes>
     );
 };
